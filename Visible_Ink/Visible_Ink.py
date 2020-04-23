6617719e40d316381d9085c1e9542a0762d4a7df
Piyushgrwl <piyushagarwal.agarwal5@gmail.com>
2020-04-23 10:16:32 +0530

    Visible Int (ID: 10)

---
+++ Visible_Ink/Visible_Ink.py
@@ -0,0 +1,17 @@
+'''
+
+Question URL: http://www.hacker.org/challenge/chal.php?id=10
+Answer URL: http://www.hacker.org/challenge/chal.php?answer=squint&id=10&go=Submit
+
+Hint: Check the source code.
+
+'''
+
+import sys
+sys.path.append('../')
+import hacker_session as hs
+
+id = 10
+answer = "squint"
+
+hs.hackerSession(answer, id)
