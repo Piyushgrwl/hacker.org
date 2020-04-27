3a37059273385cf70599f17009675a101c99e434
Piyushgrwl <piyushagarwal.agarwal5@gmail.com>
2020-04-27 19:13:56 +0530

    Steganographic (ID: 14)

---
+++ Steganographic/Steganographic.py
@@ -0,0 +1,17 @@
+'''
+
+Question URL: http://www.hacker.org/challenge/chal.php?id=14
+Answer URL: http://www.hacker.org/challenge/chal.php?answer=maniocmaniac&id=14&go=Submit
+
+Hint: Open the image in any editor and check the last chunk.
+
+'''
+
+import sys
+sys.path.append('../')
+import hacker_session as hs
+
+id = 14
+answer = "maniocmaniac"
+
+hs.hackerSession(answer, id)
