3f800a0a1cbcca032f6699eb7b5207385ae0405e
Piyushgrwl <piyushagarwal.agarwal5@gmail.com>
2020-05-03 19:59:15 +0530

    Follow the Logic (ID: 26)

---
+++ Follow_The_Logic/Follow_The_Logic.py
@@ -0,0 +1,19 @@
+'''
+
+Question URL: http://www.hacker.org/challenge/chal.php?id=26
+Answer URL: http://www.hacker.org/challenge/chal.php?answer=360&id=26&go=Submit
+
+Hint:
+1. Run Follow_the_Logic.js using NodeJS.
+2. Open Follow_the_Logic.html in a Browser.
+
+'''
+
+import sys
+sys.path.append('../')
+import hacker_session as hs
+
+id = 26
+answer = 360
+
+hs.hackerSession(answer, id)
